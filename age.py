887dbf610cfceb9722504c513beda3e0d4b102cc
pengchao2022 <pengchao.ma2@outlook.com>
2024-09-09 09:48:13 +0800

    updated python script

--- age.py
+++ age.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 
-age=34
+name = input("Please enter your name here:  ")
+
+print("Hello, " + name)
+
+year = int(input("Please enter your birth year here: "))
+
+age = 2024 - year
+
 
 print(age)
+
+
+
 
 
