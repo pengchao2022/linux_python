adb459c4c9602e551ed823ca1a1f5f41d1385d39
pengchao2022 <pengchao.ma2@outlook.com>
2024-09-09 09:54:08 +0800

    Added float number operations here

---
+++ num1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+num1 = float(input("Please enter your first number here:"))
+
+num2 = float(input("Please enter your second number here:"))
+
+sum = num1 + num2
+
+print(sum)
+
+
+
