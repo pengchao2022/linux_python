c6d251e161d8d3fa8cd5836f5164d3eb4d3ec988
pengchao2022 <pengchao.ma2@outlook.com>
2024-09-11 10:05:54 +0800

    Updated the list

--- courses.py
+++ courses.py
@@ -3,6 +3,16 @@
 
 courses = ['History', 'Math', 'Physics', 'CompSci']
 
+courses.append('Art')
+
+courses.insert(0, 'Chemistry')
+
+courses.remove('Physics')
+
+# courses.pop() will remove the last value of a list
+
+courses.pop()
+
 print(courses)
 
 print(courses[0])
@@ -14,6 +24,16 @@ print(courses[2])
 print(courses[3])
 
 print(courses[1:3])
+
+print(courses)
+
+# dismatch the orders from a list
+
+courses.reverse()
+
+print(courses)
+
+
 
 
 
