ed23333e8564f23950b736feb3f778ece6d6c361
pengchao2022 <pengchao.ma2@outlook.com>
2024-09-11 09:50:30 +0800

    Added list operation

---
+++ courses.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+courses = ['History', 'Math', 'Physics', 'CompSci']
+
+print(courses)
+
+print(courses[0])
+
+print(courses[1])
+
+print(courses[2])
+
+print(courses[3])
+
+print(courses[1:3])
+
+
+
+
+
+
+
