289226f966e1b9b9437282c99cff95ba954b2600
pengchao2022 <pengchao.ma2@outlook.com>
2024-09-11 11:52:56 +0800

    Updated the script

---
+++ dic.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+student = {'name': 'John', 'age': 25, 'courses': ['Art', 'Math']}
+
+print(student)
+
+print(len(student))
+
+print(student.items())
+
+
+for key in student:
+
+	print(key)
+
+
+
+
+
