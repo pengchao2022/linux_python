b17f29d417586fafb7dcfa26378def30cd483b74
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2024-09-12 08:04:42 +0800

    Added new scripts

---
+++ temp.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+tempstr = input("Pelase enter your temperature here also with unit symbol: ")
+
+#This is to print the second letter in the input string
+
+print(tempstr[1])
+
+#This is to print the first letter in the input string 
+
+print(tempstr[0])
+
+
+# This is to print the last letter in the input string
+
+print(tempstr[-1])
+
+#This is just for a test 
+
+print(tempstr[2])
+
+
+
