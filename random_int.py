cf3894fe573af20a8e06ce4b60f5e83103f9831a
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2024-09-12 22:57:50 +0800

    Added the random float number

--- random_int.py
+++ random_int.py
@@ -2,9 +2,23 @@
 
 import random
 
+# To get an int number between 10 and 100
+
 c = random.randint(10, 100)
 
 print(c)
+
+# To get a float umber between 10 and 100
+
+d = random.uniform(10, 100)
+
+print(d)
+
+# To get an K bit integer number
+
+e = random.getrandbits(16)
+
+print(e)
 
 
 
