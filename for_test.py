71c9a17b948bd94726139dc013a3085715025225
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2024-09-12 13:25:20 +0800

    for loop updated

--- for_test.py
+++ for_test.py
@@ -6,4 +6,19 @@ for i in range(5):
 	print("Helloworld", i)
 
 
+for a in range(10):
+
+	print("ABCDEFG", a)
+
+
+for b in range(100):
+
+	print("GOD BLESS USA", b)
+
+
+for x in range(200):
+
+	print("Designed by apple in california, Assembled in China!", x)
+
+
 
