e86763ca68bf046f495ed0e3adff6e86b641daea
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2024-09-12 13:52:59 +0800

    Added the power function

---
+++ power_fun.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+# the pow function to calculate the x raised to the y power
+
+
+
+x = 3
+
+y = 2
+
+print(pow(x,y))
+
+
+
+a = 2
+
+b = 32
+
+c = pow(a,b)
+
+print("The total IPv4 address in the world is:", c)
+
+
+
+#print(pow(a,b))
+
+
