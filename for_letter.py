83694626b9b6dcd0f193d41165387199134a97db
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2024-09-12 21:12:51 +0800

    Added the for loop in letter

---
+++ for_letter.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+for i in "Python2024":
+
+	print(i, end=",")
+ 
+print("\n")
+
+
+
+
--- range_test.py
+++ range_test.py
@@ -11,3 +11,8 @@ for a in range(1,20):
 	print(a)
 
 
+for b in "Python1234":
+
+	print(b)
+
+
