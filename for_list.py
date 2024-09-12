441c3e45cf177820acb1f6243265bea44007154d
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2024-09-12 21:20:56 +0800

    Added for list loop

---
+++ for_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+list1 = [123, "python", 900, "Hellokity", "10000"]
+
+for item in list1:
+
+	print(item, end=",")
+print("\n")
+
+
+for item in list1:
+
+	print(item)
+
+#print("\n")
+
+
+
