3922bc34e356d2439b29593bc1bd2da89d23768d
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2024-09-12 17:45:12 +0800

    Added replace function to the sentence

---
+++ count1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+sentence = "eat apple one day and you will be more healthy!"
+
+
+a1 = sentence.count("a")
+
+
+print("the letter a in the sentence pops out times is:", a1)
+
+
---
+++ replace1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+sentence = "python3"
+
+re1 = sentence.replace("n", "ooo")
+
+print(re1)
+
+
