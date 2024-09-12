33a5070d0870bc6e2e1e465184af92e664c60130
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2024-09-12 22:44:41 +0800

    Added the random function

---
+++ random_1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+import random
+
+random.seed(10)
+
+print(random.random())
+
+
+
+
---
+++ test_random1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import random
+
+
+random.seed(10)
+
+c = random.random()
+
+print(c)
+
+
