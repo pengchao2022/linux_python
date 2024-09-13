4c2e5bfaac06a4ba02a369184f553bf2f3ebbab7
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2024-09-13 11:53:43 +0800

    Added the collection function

---
+++ jihe.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+j = {1, 2, 3, 2}
+
+print(j)
+
+h = [1, 4, 33, 200, 4]
+
+print(h)
+
+
+
+# Use the set function to create the collection
+
+B = set("Python2024")
+
+print(B)
+
+
+
