f885cde2befc3e6f9a3ac6e6223b806f6dced4d1
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2024-09-13 14:48:26 +0800

    Added the dictionary operation

---
+++ dic02.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+# To define a new dictionary
+
+d = {}
+
+# To added two key and values to dictionary
+
+d["a"] = 1
+
+d["b"] = 2
+
+# check the dictionary
+
+print(d) 
+
+# modify the second element and changed to 3
+
+d["b"] = 3
+
+print(d)
+
+# To judge the c in dictonary or not
+
+print("c" in d)
+
+# To get the length of dictonary
+
+print(len(d))
+
+# To delete all the key and values in dictionary
+
+d.clear()
+
+print(d)
+
+
+
