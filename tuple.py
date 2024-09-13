3c5ff74fa3bfc68555d72ba4eaf0b3047eba54dd
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2024-09-13 13:58:20 +0800

    Updated the tuple elements

--- tuple.py
+++ tuple.py
@@ -5,4 +5,19 @@ creature = "cat", "dog", "tiger", "human"
 
 print(creature)
 
+color = (0x001100, "blue", "red", creature)
+
+print(color)
+
+#this is to reverse output elements in the tuple
+
+c = creature[::-1]
+
+print(c)
+
+d = color[::-1]
+
+print(d)
+
+
 
